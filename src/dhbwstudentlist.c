f81aa4ad9ffdad7ef2cee18d86794fd4da98d838
Erik <erik.kaufmann91@gmail.com>
2022-06-13 14:36:29 +0200

    Added different projects to solution. changed compiler to CLANG, changed files to src folder, adjusted ref for files

---
+++ src/dhbwstudentlist.c
@@ -0,0 +1,436 @@
+/*
+ ============================================================================
+ Aufgabe     : Datenstrukturen - Woche 4
+ Autor       : Erik Kaufmann
+ Matrikel    : 1390365
+ Version     : 1.0
+ ============================================================================
+ */
+#include <stdbool.h>
+#include <stdlib.h>
+#include "dhbwstudentlist.h"
+#include "dhbwstudent.h"
+
+ // DIESE METHODEN NICHT AENDERN
+
+StudentLP StudentLPAlloc(Student_p newStudent)
+{
+	StudentLP new = malloc(sizeof(StudentL));
+
+	new->student = newStudent;
+	new->next = NULL;
+
+	return new;
+}
+
+void StudentLInsertFirst(StudentLP* anchor_adr, Student_p newStudent)
+{
+	StudentLP oldFirst = *anchor_adr;
+	StudentLP newFirst = StudentLPAlloc(newStudent);
+	*anchor_adr = newFirst;
+	newFirst->next = oldFirst;
+}
+
+void StudentLInsertLast(StudentLP* anchor_adr, Student_p newStudent)
+{
+	StudentLP current = *anchor_adr;
+	StudentLP previous = NULL;
+
+	StudentLP newLP = StudentLPAlloc(newStudent);
+
+	while (current != NULL)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (previous != NULL)
+	{
+		previous->next = newLP;
+	}
+	else
+	{
+		*anchor_adr = newLP;
+	}
+}
+
+int StudentLSize(StudentLP* anchor_adr)
+{
+	StudentLP current = *anchor_adr;
+
+	int size = 0;
+
+	while (current != NULL)
+	{
+		size++;
+		current = current->next;
+	}
+
+	return size;
+}
+
+void StudentLPFree(StudentLP info)
+{
+	if (info == NULL)
+		return;
+
+	StudentFree(info->student);
+	free(info);
+	return;
+}
+
+void StudentLFree(StudentLP* anchor_adr)
+{
+	if (anchor_adr == NULL)
+	{
+		return;
+	}
+
+	StudentLP current = *anchor_adr;
+
+	while (current)
+	{
+		StudentLP newCurrent = current->next;
+		StudentLPFree(current);
+		current = newCurrent;
+	}
+
+	*anchor_adr = NULL;
+	return;
+}
+
+StudentLP deepLPCopy(StudentLP info)
+{
+	if (info == NULL)
+		return NULL;
+	StudentLP copy = StudentLPAlloc(deepCopy(info->student));
+	copy->next = NULL;
+	return copy;
+}
+
+StudentLP* deepLCopy(StudentLP* anchor_adr)
+{
+	if (anchor_adr == NULL)
+		return NULL;
+
+	StudentLP* copy = malloc(sizeof(StudentLP));
+	*copy = NULL;
+
+	StudentLP current = *anchor_adr;
+
+	while (current)
+	{
+		StudentLInsertLast(copy, deepCopy(current->student));
+		current = current->next;
+	}
+
+	return copy;
+}
+
+StudentLP* StudentsFromFile(char* filename)
+{
+	FILE* in = fopen(filename, "r");
+
+	char string[BUF_SIZE];
+	StudentLP* all_students = malloc(sizeof(StudentLP));
+	*all_students = NULL;
+	StudentLP reverse_students_anchor = NULL;
+	StudentLP* reverse_students = &reverse_students_anchor;
+
+	while (fgets(string, BUF_SIZE, in))
+	{
+		// remove newline (works for both windows and unix)
+		string[strcspn(string, "\r\n")] = 0;
+
+		// printf("%s\n", string);
+		StudentLInsertFirst(reverse_students, StudentAlloc(string));
+	}
+
+	// create normal ordered list
+	{
+		StudentLP current = *reverse_students;
+		while (current != NULL)
+		{
+			StudentLInsertFirst(all_students, deepCopy(current->student));
+			current = current->next;
+		}
+	}
+
+	StudentLFree(reverse_students);
+
+	return all_students;
+}
+
+// Bis hier nicht �ndern
+
+// Ab hier Aufgaben
+
+bool StudentLImplemented()
+{
+	// TODO: hier auf true aendern, damit Ihre Implementierung getestet wird
+	return true;
+}
+
+bool StudentLContainsStudent(StudentLP* anchor_adr, Student_p student)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+
+	// if list is empty
+	if (current == NULL)
+		return NULL;
+
+	// iterate over linked list
+	while (current)
+	{
+		Student_p currentStud = current->student;
+
+		if (strcmp(currentStud->lastname, student->lastname) == 0 && currentStud->matrnr == student->matrnr)
+		{
+			// lastname is equal
+			// matrnr is equal
+			// return current;
+			return true;
+		}
+
+		// NULL if reached the tail
+		if (current->next == NULL)
+			return NULL;
+		else
+			// get next
+			current = current->next;
+	}
+
+	return false;
+}
+
+StudentLP StudentLExtractStudent(StudentLP* anchor_adr, Student_p student)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+	StudentLP before = NULL;
+
+	// if list is empty
+	if (current == NULL)
+		return NULL;
+
+	// iterate over linked list
+	while (current)
+	{
+		if (strcmp(current->student->lastname, student->lastname) == 0
+			&& current->student->matrnr == student->matrnr)
+		{
+			// lastname is equal
+			// matrnr is equal
+			// extract
+			StudentLP temp = current;
+
+			if (before)
+			{
+				before->next = current->next;
+			}
+			else if (current->next)
+			{
+				current = current->next;
+				*anchor_adr = current;
+			}
+			else
+			{
+				*anchor_adr = NULL;
+			}
+
+			return temp;
+		}
+
+		// NULL if reached the tail
+		if (current->next == NULL)
+		{
+			return NULL;
+		}
+		else
+		{
+			// get next
+			before = current;
+			current = current->next;
+		}
+	}
+}
+
+StudentLP StudentLFindStudent(StudentLP* anchor_adr, Student_p student)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+
+	// if list is empty
+	if (current == NULL)
+		return NULL;
+
+	// iterate over linked list
+	while (current)
+	{
+		Student_p currentStud = current->student;
+
+		if (strcmp(currentStud->lastname, student->lastname) == 0 && currentStud->matrnr == student->matrnr)
+		{
+			// lastname is equal
+			// matrnr is equal
+			// return current;
+			return current;
+		}
+
+		// NULL if reached the tail
+		if (current->next == NULL)
+			return NULL;
+		else
+			// get next
+			current = current->next;
+	}
+}
+
+StudentLP StudentLFindByMatr(StudentLP* anchor_adr, int matrnr)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+
+	// if list is empty
+	if (current == NULL)
+		return NULL;
+
+	// iterate over linked list
+	while (current)
+	{
+		Student_p currentStud = current->student;
+
+		if (currentStud->matrnr == matrnr)
+			// matrnr is equal
+			return current;
+
+		// NULL if reached the tail
+		if (current->next == NULL)
+			return NULL;
+		else
+			// get next
+			current = current->next;
+	}
+}
+
+StudentLP StudentLFindByName(StudentLP* anchor_adr, char* lastname)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+
+	// if list is empty
+	if (current == NULL)
+		return NULL;
+
+	// iterate over linked list
+	while (current)
+	{
+		Student_p currentStud = current->student;
+
+		if (strcmp(currentStud->lastname, lastname) == 0)
+			// matrnr is equal
+			return current;
+
+		// NULL if reached the tail
+		if (current->next == NULL)
+			return NULL;
+		else
+			// get next
+			current = current->next;
+	}
+}
+
+void StudentLInsertSorted(StudentLP* anchor_adr, Student_p newStudent)
+{
+	// start from the first link
+	StudentLP current = *anchor_adr;
+	StudentLP before = NULL;
+	StudentLP newStudentLP = NULL;
+
+	if (newStudent->matrnr == 0)
+	{
+		// skip corrupt data
+		// there should be no entry with matrNr == 0 
+		return;
+	}
+
+	if (*anchor_adr == NULL)
+	{
+		// use existing method to insertFirst
+		StudentLInsertFirst(anchor_adr, newStudent);
+		return;
+	}
+
+	// iterate over linked list
+	// as long as current not null or nullptr
+	while (current)
+	{
+		if (current->next == NULL) // If current is tail
+		{
+			if (newStudent->matrnr < current->student->matrnr)
+			{
+				newStudentLP = StudentLPAlloc(newStudent);
+
+				if (before == NULL)
+				{
+					// Set newStudent as new head of list
+					StudentLP oldHead = *anchor_adr;
+					*anchor_adr = newStudentLP;
+					newStudentLP->next = oldHead;
+				}
+				else
+				{
+					// place in between
+					before->next = newStudentLP;
+				}
+			}
+			else
+			{
+				if (StudentLContainsStudent(anchor_adr, newStudent))
+				{
+					//avoid redundancy
+					return;
+				}
+				else
+				{
+					StudentLInsertLast(anchor_adr, newStudent);
+				}
+			}
+
+			return;
+		}
+		else // not tail
+		{
+			newStudentLP = StudentLPAlloc(newStudent);
+
+			if (current->student->matrnr < newStudent->matrnr && current->next->student->matrnr > newStudent->matrnr)
+			{
+				newStudentLP->next = current->next;
+				current->next = newStudentLP;
+				return;
+			}
+
+			if (newStudent->matrnr < current->student->matrnr)
+			{
+				if (before == NULL) // current is head
+				{
+					StudentLInsertFirst(anchor_adr, newStudent);
+				}
+				else
+				{
+					before->next = newStudentLP;
+					newStudentLP->next = current;
+				}
+
+				return;
+			}
+			else
+			{
+				// set before and get next
+				before = current;
+				current = current->next;
+			}
+		}
+	}
+}
---
+++ src/dhbwstudenttree.c
@@ -0,0 +1,344 @@
+﻿/*
+ ============================================================================
+ Aufgabe     : Datenstrukturen
+ Autor       : Erik Kaufmann
+ Matrikel    : 1390365
+ Version     : 1.0
+ ============================================================================
+ */
+#include <stdbool.h>
+#include <stdlib.h>
+#include "dhbwstudent.h"
+#include "dhbwstudenttree.h"
+
+ // DIESE METHODEN NICHT AENDERN
+
+StudentTP StudentTPAlloc(Student_p newStudent)
+{
+	StudentTP new = malloc(sizeof(StudentT));
+
+	new->student = newStudent;
+	new->lchild = NULL;
+	new->rchild = NULL;
+
+	return new;
+}
+
+void StudentTPFree(StudentTP tree)
+{
+	if (tree == NULL)
+		return;
+
+	StudentFree(tree->student);
+	free(tree);
+	return;
+}
+
+void StudentTFree(StudentTP* root_adr)
+{
+
+	StudentTP current = *root_adr;
+
+	if (current == NULL)
+	{
+		return;
+	}
+	StudentTFree(&(current->lchild));
+	StudentTFree(&(current->rchild));
+
+	StudentTPFree(current);
+	return;
+}
+
+StudentTP deepTPCopy(StudentTP info)
+{
+	if (info == NULL)
+		return NULL;
+	StudentTP copy = StudentTPAlloc(deepCopy(info->student));
+	copy->lchild = NULL;
+	copy->rchild = NULL;
+	return copy;
+}
+
+// Bis hier nicht �ndern
+
+// Ab hier Aufgaben
+
+bool StudentTImplemented()
+{
+	// TODO: hier auf true aendern, damit Ihre Implementierung getestet wird
+	return true;
+}
+
+bool StudentTContainsStudent(StudentTP* root_adr, Student_p student)
+{
+	// set root adr
+	StudentTP root = *root_adr;
+	bool foundStudent = false;
+
+	if (root->student->matrnr == student->matrnr) // gleich
+	{
+		//printf("<Duplicate student %s %d >\n", student->lastname, student->matrnr);
+		return true;
+	}
+	else if (root->student->matrnr > student->matrnr) // left sub tree
+	{
+		root = root->lchild;
+	}
+	else // right sub tree
+	{
+		root = root->rchild;
+	}
+
+	if (root != NULL)
+	{
+		foundStudent = StudentTContainsStudent(&root, student);
+	}
+
+	return foundStudent;
+}
+
+
+StudentTP StudentTFindByMatr(StudentTP* root_adr, int matrnr)
+{
+	if (*root_adr == NULL)
+	{
+		return NULL;
+	}
+
+	StudentTP foundStudent = NULL;
+
+	if ((*root_adr)->student->matrnr == matrnr)
+	{
+		foundStudent = (*root_adr);
+	}
+	else
+	{
+		foundStudent = StudentTFindByMatr(&(*root_adr)->lchild, matrnr);
+
+		if (foundStudent == NULL)
+			foundStudent = StudentTFindByMatr(&(*root_adr)->rchild, matrnr);
+
+	}
+
+	return foundStudent;
+}
+
+StudentTP StudentTFindByName(StudentTP* root_adr, char* lastname)
+{
+	if (*root_adr == NULL)
+	{
+		return NULL;
+	}
+
+	StudentTP foundStudent = NULL;
+
+	if (strcmp((*root_adr)->student->lastname, lastname) == 0)
+	{
+		foundStudent = (*root_adr);
+	}
+	else
+	{
+		foundStudent = StudentTFindByName(&(*root_adr)->lchild, lastname);
+
+		if (foundStudent == NULL)
+			foundStudent = StudentTFindByName(&(*root_adr)->rchild, lastname);
+	}
+
+	return foundStudent;
+}
+
+bool hasTwoSubRefs(StudentT* node)
+{
+	return (node->lchild != NULL && node->rchild != NULL);
+}
+
+bool deleteFromTree(StudentTP* root, StudentT* targetingNode)
+{
+	bool contained = false;
+
+	if (root == NULL)
+	{
+		return false; // cant remove, tree is empty
+	}
+	else
+	{
+		// check if tree contains targetingNode
+		contained = StudentTContainsStudent(root, targetingNode->student);
+
+		if (contained) // continue
+		{
+			// adjust pointers
+			if ((*root)->student->matrnr == targetingNode->student->matrnr) // is root
+			{
+				// complicated
+			}
+			else // not root, check if targetingNode has 2 refs (left, right
+			{
+				if (hasTwoSubRefs(targetingNode))
+				{
+					// true --> complicated
+				}
+				else
+				{
+					// has only 1 ref --> readjust ref
+
+					if (targetingNode->lchild == NULL)
+					{
+						// right tree
+						//targetingNode->rchild
+					}
+				}
+			}
+		}
+		else // not existing in tree
+		{
+			return false;
+		}
+	}
+}
+
+void getNext(StudentLP* headOfList, StudentT* currentTreeElement)
+{
+	if (!currentTreeElement)
+		return;
+
+	StudentT* currentTreeStud = currentTreeElement;
+
+	StudentLInsertSorted(headOfList, currentTreeElement->student);
+
+	getNext(headOfList, currentTreeElement->lchild);
+	getNext(headOfList, currentTreeElement->rchild);
+}
+
+StudentLP* StudentTToSortedList(StudentTP* root_adr)
+{
+	StudentLP headOfListAnchor = NULL;
+	StudentLP* headOfList = &headOfListAnchor;
+
+	if (root_adr == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		StudentLInsertSorted(headOfList, (*root_adr)->student);
+	}
+
+	getNext(headOfList, *root_adr);
+
+	return deepLCopy(headOfList);
+}
+
+bool insertSorted(StudentT* studentNode, Student_p newStudent)
+{
+	bool ret = false;
+
+	if (studentNode == NULL)
+	{
+		return true; // parentNode will be new StudentNode
+	}
+
+	else if ((studentNode)->student->matrnr > newStudent->matrnr) // left tree
+	{
+		ret = insertSorted((studentNode)->lchild, newStudent);
+		if (ret)
+		{
+			if (StudentTContainsStudent(&studentNode, newStudent))
+			{
+				return false; // student already exists
+			}
+			StudentTP newChild = StudentTPAlloc(newStudent);
+			(studentNode)->lchild = newChild;
+		}
+	}
+	else
+	{
+		ret = insertSorted((studentNode)->rchild, newStudent);
+		if (ret)
+		{
+			if (StudentTContainsStudent(&studentNode, newStudent))
+			{
+				return false; // student exists
+			}
+			StudentTP newChild = StudentTPAlloc(newStudent);
+			(studentNode)->rchild = newChild;
+		}
+	}
+}
+
+void StudentTInsertSorted(StudentTP* root_adr, Student_p newStudent)
+{
+	StudentTP current = *root_adr;
+	if (current == NULL) // insertFirst
+	{
+		*root_adr = StudentTPAlloc(newStudent);
+	}
+	else
+	{
+		insertSorted(current, newStudent);
+	}
+}
+
+int getTreeSize(StudentT* studentNode)
+{
+	int counter = 0;
+
+	if (studentNode == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		counter = getTreeSize(studentNode->lchild) + getTreeSize(studentNode->rchild);
+	}
+
+	return counter + 1;
+}
+
+int StudentTSize(StudentTP* root_adr)
+{
+	int counter = 0;
+
+	StudentTP current = *root_adr;
+
+	if (*root_adr == NULL)
+		return 0;
+	else
+	{
+		counter = getTreeSize(current);
+	}
+	// Return length of tree
+	// amount of all elements under the tree
+
+	return counter;
+}
+
+int getDepth(StudentT* studentNode)
+{
+	if (studentNode == NULL)
+		return 0;
+
+	int tempDepthLeft = getDepth(studentNode->lchild);
+	int tempDepthRight = getDepth(studentNode->rchild);
+
+	/* use the larger one */
+	if (tempDepthLeft > tempDepthRight)
+		return (tempDepthLeft + 1);
+	else
+		return (tempDepthRight + 1);
+}
+
+int StudentTDepth(StudentTP* root_adr)
+{
+	int maxDepth = 0;
+
+	if (root_adr == NULL)
+		return 0;
+	else
+	{
+		maxDepth = getDepth((*root_adr));
+	}
+
+	return maxDepth;
+}
